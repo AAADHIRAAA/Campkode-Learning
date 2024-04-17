6df64bc41fa2b9cb10845090749093847b6d6d5b
Sovereign-Sairam <saiarudra2004@gmail.com>
2024-04-17 15:58:12 +0530

    Point card

---
+++ my-app/components/PointCard/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+
+function index() {
+  return (
+    <div class="point-card" data-aos="zoom-out" data-aos-delay="100">
+        <h3>VISION</h3>
+        <hr class="underline underline-points" />
+        <p>CampKode was found</p>
+    </div>
+  )
+}
+
+export { index as PointCard }
---
+++ my-app/components/PointCard/mission.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+
+function index() {
+  return (
+    <div class="point-card" data-aos="zoom-out" data-aos-delay="100">
+        <h3>MISSION</h3>
+        <hr class="underline underline-points" />
+        <p>CampKode was given a mission</p>
+    </div>
+  )
+}
+
+export { index as Mission }
